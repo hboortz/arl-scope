68e902e239112ec7f03af233f8c82b869f92baa6
Charles Goddard and Kyle McConnaughay <chargoddard+kyle.mcconnaughay@gmail.com>
2014-09-24 16:55:51 -0400

    Read from camera number 0

--- opencv_publisher_eric/src/test_video_publish.py
+++ opencv_publisher_eric/src/test_video_publish.py
@@ -6,8 +6,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class image_converter:
-
+class ImageConverter(object):
   def __init__(self):
     self.image_pub = rospy.Publisher("video_topic", Image, queue_size=10)
 
@@ -15,28 +14,7 @@ class image_converter:
     self.bridge = CvBridge()
     # self.image_sub = rospy.Subscriber("image_topic",Image,self.callback)
 
-    self.cap = cv2.VideoCapture(1)
-    # self.cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 400);
-    # self.cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 400);
-    # self.cap.set(cv2.cv.CV_CAP_PROP_FPS, 60);
-
-  # def callback(self,data):
-  #   try:
-  #     cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-  #   except CvBridgeError, e:
-  #     print e
-
-  #   (rows,cols,channels) = cv_image.shape
-  #   if cols > 60 and rows > 60 :
-  #     cv2.circle(cv_image, (50,50), 10, 255)
-
-  #   cv2.imshow("Image window", cv_image)
-  #   cv2.waitKey(3)
-
-  #   try:
-  #     self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-  #   except CvBridgeError, e:
-  #     print e
+    self.cap = cv2.VideoCapture(0)
 
 def main(args):
   ic = image_converter()
@@ -54,7 +32,6 @@ def main(args):
 
       # Display the resulting frame
       cv2.imshow('frame',frame)
-      cv2.imshow('gray',gray)
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
