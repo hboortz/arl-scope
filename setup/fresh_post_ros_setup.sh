c0ee81e8be09cdc211b34f508ba2070f1a63e2e8
eschneider1992 <franz.eric.schneider@gmail.com>
2014-09-29 22:19:26 -0400

    Update fresh_post_ros_setup.sh

--- setup/fresh_post_ros_setup.sh
+++ setup/fresh_post_ros_setup.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# Install OpenCV
-# http://docs.opencv.org/doc/tutorials/introduction/linux_install/linux_install.html#linux-installation
-# cd
-# if [ ! -d "opencv" ]; then
-#	git clone https://github.com/Itseez/opencv.git
-#	cd ~/opencv
-#	mkdir release
-#	cd release
-#	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
-#	make
-#	sudo make install
-#fi
-
-
 # Install ARL stuff
 cd ~/catkin_ws/src
 if [ ! -d "arl-scope" ]; then
