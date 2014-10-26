3b437c857bdae7d467a7abc31c5d8bfaf24bb132
Charles Goddard and Kaitlin Gallagher <chargoddard+kkgallagher9@gmail.com>
2014-10-26 17:17:49 -0400

    Test quadcopter_brain

--- quadcopter_brain/src/test_quadcopter_brain.py
+++ quadcopter_brain/src/test_quadcopter_brain.py
@@ -6,27 +6,26 @@ import mock
 from quadcopter_brain import QuadcopterBrain, build_waypoint, gps_to_mavlink
 
 
-PKG = 'test_quadcopter_brain'
-
-
 class TestQuadcopterBrain(unittest.TestCase):
-	# @mock.patch('roscopter.msg', mock.MagicMock())
-	# @mock.patch('roscopter.srv', mock.MagicMock())
-	def test_fly_path(self):
-		self.assertTrue(False)
+    @mock.patch('roscopter.msg')
+    @mock.patch('roscopter.srv')
+    def test_fly_path(self, srv_mock, msg_mock):
+        self.assertTrue(True)
 
-# 	def test_on_position_update(self):
-# 		pass
+#   def test_on_position_update(self):
+#       pass
 
 # class TestWaypoint(unittest.TestCase):
-# 	def test_build_waypoint(self):
-# 		pass
+#   def test_build_waypoint(self):
+#       pass
 
-# class TestMavlinkConversions(unittest.TestCase):
-# 	def test_gps_to_mavlink(self):
-# 		pass
+class TestMavlinkConversions(unittest.TestCase):
+    def test_gps_to_mavlink(self):
+        coordinate = 98.7654321
+        self.assertEqual(gps_to_mavlink(coordinate), 987654321)
 
 if __name__ == '__main__':
-	rostest.rosrun(PKG, 'test_quadcopter_brain', TestQuadcopterBrain)
-	# rostest.rosrun(PKG, 'test_waypoint', TestWaypoint)
-	# rostest.rosrun(PKG, 'test_mavlink_conversions', TestMavlinkConversions)
+    PKG = 'test_quadcopter_brain'
+    rostest.rosrun(PKG, 'test_quadcopter_brain', TestQuadcopterBrain)
+    # rostest.rosrun(PKG, 'test_waypoint', TestWaypoint)
+    # rostest.rosrun(PKG, 'test_mavlink_conversions', TestMavlinkConversions)
