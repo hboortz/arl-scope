5ba5f437b0596c21a8eab8ff82a783e635e4f215
Charles Goddard and Kaitlin Gallagher <chargoddard+kkgallagher9@gmail.com>
2015-01-28 18:35:23 -0500

    Fixed typos and formatting before pull request

--- quadcopter_brain/src/latlon_test.py
+++ quadcopter_brain/src/latlon_test.py
@@ -5,6 +5,7 @@ from quadcopter_fiducial_brain import QuadcopterFiducialBrain
 from landing_site import LandingSite
 import geodesy.utm
 
+
 def latlon_diff(latA, lonA, latB, lonB):
     pointA = geodesy.utm.fromLatLong(latA, lonA)
     pointB = geodesy.utm.fromLatLong(latB, lonB)
@@ -15,6 +16,7 @@ def latlon_diff(latA, lonA, latB, lonB):
     print "Differences (m): %f, %f" % (dX, dY)
     print "Distance (m): ", (dX**2 + dY**2)**0.5
 
+
 def main():
     derpy = QuadcopterFiducialBrain()
     moon = LandingSite()
@@ -22,11 +24,12 @@ def main():
     derpy.latitude = 42.0
     derpy.longitude = -71.0
     derpy.heading = 0
-    
+
     moon.center = Pose(position=Point(x=6, y=6, z=6))
 
-    lat,lon = moon.latlon(derpy)
+    lat, lon = moon.latlon(derpy)
     latlon_diff(derpy.latitude, derpy.longitude, lat, lon)
+
 
 if __name__ == '__main__':
     main()
