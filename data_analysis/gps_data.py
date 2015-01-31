25fda894237e4a5fbad6e4dd8debd558dfaf65fd
Charles Goddard and Kaitlin Gallagher <chargoddard+kkgallagher9@gmail.com>
2015-01-31 15:05:59 -0500

    Normalize times and ignore title row in csv files

--- data_analysis/gps_data.py
+++ data_analysis/gps_data.py
@@ -8,11 +8,13 @@ from filtered_pos import FilteredPos
 def load_gps_data(filename):
     with open(filename, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=",")
-        return [FilteredPos(row) for row in csvreader]
+        reader.next()
+        return [FilteredPos(row) for row in reader]
 
 def extract_gps_coordinates(filtered_poses):
     return numpy.array([(each.latitude, 
                          each.longitude) for each in filtered_poses])
 
 def extract_gps_times(filtered_poses):
-    return numpy.array([each.time for each in filtered_poses])
+    times = numpy.array([each.time for each in filtered_poses])
+    return times - filtered_poses[0].time
