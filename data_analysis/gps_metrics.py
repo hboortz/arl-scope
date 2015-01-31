fc6beb5de65e4ac319ca1032749ca3a43afae213
Charles Goddard and Kaitlin Gallagher <chargoddard+kkgallagher9@gmail.com>
2015-01-31 15:06:36 -0500

    Compute distance between points in meters

--- data_analysis/gps_metrics.py
+++ data_analysis/gps_metrics.py
@@ -1,4 +1,5 @@
 import numpy
+import geodesy.utm
 
 
 def center_of_gravity(data):
@@ -6,10 +7,17 @@ def center_of_gravity(data):
     average_y = numpy.mean([point[1] for point in data])
     return numpy.array([average_x, average_y])
 
+def latlon_diff(latA, lonA, latB, lonB):
+    pointA = geodesy.utm.fromLatLong(latA, lonA)
+    pointB = geodesy.utm.fromLatLong(latB, lonB)
+
+    dX = pointB.easting - pointA.easting
+    dY = pointB.northing - pointA.northing
+
+    return (dX**2 + dY**2)**0.5
 
 def euclidean_distance(p1, p2):
-    return numpy.linalg.norm(p1 - p2)
-
+    return latlon_diff(p1[0], p1[1], p2[0], p2[1])
 
 def precision(data):
     cog = center_of_gravity(data)
@@ -27,3 +35,6 @@ def speeds(times, data):
     distances = [euclidean_distance(p1, p2) for p1, p2 in zip(data[1:], 
                                                               data[:-1])]
     return [distance / time for distance, time in zip(distances, delta_times)]
+
+def average_speed(times, data):
+    return numpy.mean(speeds(times, data))
