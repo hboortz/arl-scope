8f676bbcb54cf3eb578d2a8cdc6c6377b1722a4a
Charles Goddard and Kaitlin Gallagher <chargoddard+kkgallagher9@gmail.com>
2015-01-31 15:07:25 -0500

    Compute GPS statistics

--- data_analysis/gps_characterization.py
+++ data_analysis/gps_characterization.py
@@ -1,28 +1,21 @@
-import numpy
+import os
+
+import gps_data
+import gps_metrics 
+
+def main():
+    user = os.environ["USER"]
+    filename = ("/home/" + user + "/catkin_ws/src/arl-scope/bagfiles"
+                "/carl_greatlawn_bottomleftconner_2015-01-28-15-32-40.csv")
+    data = gps_data.load_gps_data(filename)
+    gps_coordinates = gps_data.extract_gps_coordinates(data)
+    gps_times = gps_data.extract_gps_times(data)
+
+    print gps_metrics.center_of_gravity(gps_coordinates)
+    print gps_metrics.precision(gps_coordinates)
+    print gps_metrics.average_speed(gps_times, gps_coordinates)
 
 
-def center_of_gravity(data):
-    average_x = numpy.mean([point[0] for point in data])
-    average_y = numpy.mean([point[1] for point in data])
-    return numpy.array([average_x, average_y])
 
-
-def euclidean_distance(p1, p2):
-    return numpy.linalg.norm(p1 - p2)
-
-
-def precision(data):
-    cog = center_of_gravity(data)
-    distances = numpy.array([euclidean_distance(point, cog) for point in data])
-    return numpy.mean(distances)
-
-
-def simple_moving_average(data, window=5):
-    weights = numpy.repeat(1.0, window) / window
-    return numpy.convolve(data, weights, 'valid')
-
-
-def speeds(times, data):
-    delta_times = times[1:] - times[:-1]
-    distances = [euclidean_distance(p1, p2) for p1, p2 in zip(data[1:], data[:-1])]
-    return [distance / time for distance, time in zip(distances, delta_times)]
+if __name__ == '__main__':
+    main()
