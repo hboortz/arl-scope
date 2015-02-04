73a248d1a5f3e3c4c0fc97a6b3ca844bb8b5fb44
arl-tower-one <arl.olin.scope@gmail.com>
2015-02-04 13:01:53 -0500

    Plot GPS data

--- data_analysis/gps_characterization.py
+++ data_analysis/gps_characterization.py
@@ -21,52 +21,30 @@ def get_filename():
     return filename
 
 
-def plot_measured_gps(ax, latitudes, longitudes):
-    param_dict = {'marker': 'o',
-                  'color': 'green'}
-    plotting.plot_xy_coordinates(ax, latitudes, longitudes, param_dict)
-
-def plot_true_gps(ax, latitude, longitude):
-    param_dict = {'marker': 'o',
-                  'color': 'red',
-                  'markersize': 16}
-    plotting.plot_xy_coordinates(ax, latitude, longitude, param_dict)
-
-
 def plot_gps_coordinates_in_meters(measured_gps, true_gps):
-    fig, ax = plt.subplots(1, 1)
-    ax.set_xlabel('Latitude (m)')
-    ax.set_ylabel('Longitude (m)')
-    ax.set_title('Raised Stationary GPS Test (2)')
     measured_gps = lat_lon_to_meters(measured_gps)
     true_gps = lat_lon_to_meters([true_gps])
-    plot_measured_gps(ax, measured_gps[0], measured_gps[1])
-    plot_true_gps(ax, true_gps[0], true_gps[1])
-    plt.show()
 
-
-def plot_gps_coordinates_in_latlon(measured_gps, true_gps):
     fig, ax = plt.subplots(1, 1)
-    plot_measured_gps(ax, measured_gps[:,0], measured_gps[:,1])
-    plot_true_gps(ax, true_gps[0], true_gps[1])
-    
+    plotting.plot_xy_coordinates(
+        ax, measured_gps[0], measured_gps[1], {'marker': 'o', 'color': 'green'}
+    )
+    plotting.plot_xy_coordinates(
+        ax, true_gps[0], true_gps[1],
+        {'marker': 'o', 'color': 'red', 'markersize': 16}
+    )
+    plotting.label_axes(
+        ax, 'Raised Stationary GPS Test (2)', 'Latitude (m)', 'Longitude (m)'
+    )
     plt.show()
+
 
 def lat_lon_to_meters(gps_points):
-    print gps_points
-
-    x_metered_points = []
-    y_metered_points = []
-
-    for lat, lon in gps_points:
-        metered_point = geodesy.utm.fromLatLong(lat, lon)
-        x = metered_point.northing
-        y = metered_point.easting
-        x_metered_points.append(x)
-        y_metered_points.append(y)
+    utm_points = [geodesy.utm.fromLatLong(lat, lon) for lat, lon in gps_points]
+    x_metered_points = [point.northing for point in utm_points]
+    y_metered_points = [point.easting for point in utm_points]
 
     return (x_metered_points, y_metered_points)
-
 
 
 def main():
@@ -75,7 +53,6 @@ def main():
     gps_times = gps_data.extract_gps_times(data)
     #print_metrics(gps_coordinates, gps_times)
     plot_gps_coordinates_in_meters(gps_coordinates, (42.292050, -71.262735))
-
 
 
 if __name__ == '__main__':
--- data_analysis/plotting.py
+++ data_analysis/plotting.py
@@ -1,2 +1,8 @@
 def plot_xy_coordinates(ax, xs, ys, param_dict={}):
-	return ax.plot(xs, ys, **param_dict)
+    return ax.plot(xs, ys, **param_dict)
+
+
+def label_axes(ax, title, xlabel, ylabel):
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
