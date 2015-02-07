c496abc0621e63dea59abef2d437b50c0d872636
Heather <heather.boortz@gmail.com>
2015-02-07 14:08:01 -0500

    adding custom exception for FlightError

---
+++ quadcopter_brain/src/flight_error.py
@@ -0,0 +1,12 @@
+class FlightError(Exception):
+    def __init__(self, message="", copter=None):
+        self.msg = message
+        if copter:
+            self.hover_in_place(copter)
+    
+    def __str__(self):
+        return self.msg
+
+    def hover_in_place(self, copter):
+        copter.hover_in_place()
+
