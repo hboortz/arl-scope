e4cbb80620451f37d349e1116fb00bb86cf8b791
Eric Schneider and Kyle McConnaughay <franz.eric.schneider+kyle.mcconnaughay@gmail.com>
2015-02-24 14:38:51 -0500

    Remove duplicate fly_path function definition

--- quadcopter_brain/src/quadcopter_brain/quadcopter_brain.py
+++ quadcopter_brain/src/quadcopter_brain/quadcopter_brain.py
@@ -55,8 +55,3 @@ class QuadcopterBrain(object):
             return dist_from_waypoint < error_margin
         except AttributeError:  # if haven't gotten current position data
             return False
-
-    def fly_path(self, waypoint_data):
-        self.launch()
-        self.go_to_waypoints(waypoint_data)
-        self.land()
