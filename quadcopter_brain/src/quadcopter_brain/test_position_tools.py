04a72c007992af4af46627e8e11913afe53ae73f
Eric Schneider and Kyle McConnaughay <franz.eric.schneider+kyle.mcconnaughay@gmail.com>
2015-02-24 16:25:40 -0500

    Test PositionTools.metered_offset

--- quadcopter_brain/src/quadcopter_brain/test_position_tools.py
+++ quadcopter_brain/src/quadcopter_brain/test_position_tools.py
@@ -29,6 +29,19 @@ class TestPositionTools(unittest.TestCase):
         self.assertEqual(dY, -40)
         self.assertAlmostEqual(distance, math.sqrt(2000))
 
+    def test_metered_offset(self):
+        # Positions taken from google maps
+        new_position =\
+            PositionTools.metered_offset(42.293036, -71.263321, 0, -14.78)
+        # 5 decimal places is less than a meter
+        self.assertAlmostEqual(new_position[0], 42.292903, places=5)
+        self.assertAlmostEqual(new_position[1], -71.263319, places=5)
+        new_position =\
+            PositionTools.metered_offset(42.293009, -71.263531, 157.65, 0)
+        # Lower accuracy because of the larger displacement (150m)
+        self.assertAlmostEqual(new_position[0], 42.293010, places=4)
+        self.assertAlmostEqual(new_position[1], -71.261616, places=4)
+
     def test_gps_to_mavlink(self):
         mavlink_gps = PositionTools.gps_to_mavlink(42.0)
         self.assertEqual(mavlink_gps, 420000000)
