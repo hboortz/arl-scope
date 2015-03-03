27610238fe318ba70ce56d9bd280f4700a5e6111
Eric Schneider and Kyle McConnaughay <franz.eric.schneider+kyle.mcconnaughay@gmail.com>
2015-03-03 15:13:08 -0500

    Pep8

--- quadcopter_brain/src/quadcopter_brain/test_waypoint_tools.py
+++ quadcopter_brain/src/quadcopter_brain/test_waypoint_tools.py
@@ -10,21 +10,25 @@ from waypoint_tools import WaypointTools
 
 class TestWaypointTools(unittest.TestCase):
     def test_build_waypoint(self):
-        waypoint_data = {"latitude":42.12345,
-                         "longitude":-71.98765}
+        waypoint_data = {"latitude": 42.12345,
+                         "longitude": -71.98765}
         waypoint = WaypointTools.build_waypoint(waypoint_data)
         self.assertEqual(type(waypoint), roscopter.msg.Waypoint)
         self.assertAlmostEqual(waypoint.latitude, 421234500)
         self.assertAlmostEqual(waypoint.longitude, -719876500)
+        self.assertEqual(waypoint.altitude, 8000)
+        self.assertEqual(waypoint.hold_time, 3000)
 
-        waypoint_data = {"latitude":42.12345,
-                         "longitude":-71.98765,
-                         "altitude":1.0}
+        waypoint_data = {"latitude": 42.12345,
+                         "longitude": -71.98765,
+                         "altitude": 1.0, 
+                         "hold_time": 10.0}
         waypoint = WaypointTools.build_waypoint(waypoint_data)
         self.assertEqual(type(waypoint), roscopter.msg.Waypoint)
         self.assertEqual(waypoint.latitude, 421234500)
         self.assertEqual(waypoint.longitude, -719876500)
         self.assertEqual(waypoint.altitude, 1000)
+        self.assertEqual(waypoint.hold_time, 10000)
 
     def test_open_waypoint_file(self):
         A = {"A": {"latitude": 42.292695, "longitude": -71.263091}}
