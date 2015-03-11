1d68a0f5d38101134b781e4406feda6e1588bfde
Kyle McConnaughay <kyle.mcconnaughay@gmail.com>
2015-03-11 17:46:33 -0400

    Refactor RCCommand to take a parameter dictionary to encourage its use as an immutable type

--- quadcopter_brain/src/quadcopter_brain/rc_command.py
+++ quadcopter_brain/src/quadcopter_brain/rc_command.py
@@ -2,51 +2,47 @@ import numpy
 
 
 class RCCommand(object):
-    def __init__(self):
+    def __init__(self, parameters={}):
         self._NO_CHANGE = 65535
-        
-        self._roll = 1500
+
         self._roll_min_pwm = 1100
         self._roll_max_pwm = 1900
+        self._roll = self.compute_pwm(
+            parameters.get("roll", 0.5), 
+            self._roll_min_pwm, 
+            self._roll_max_pwm)
         
-        self._pitch = 1500
         self._pitch_min_pwm = 1100
         self._pitch_max_pwm = 1900
+        self._pitch = self.compute_pwm(
+            parameters.get("pitch", 0.5), 
+            self._pitch_min_pwm, 
+            self._pitch_max_pwm)
         
-        self._yaw = 1500
         self._yaw_min_pwm = 1100
         self._yaw_max_pwm = 1900
+        self._yaw = self.compute_pwm(
+            parameters.get("yaw", 0.5), 
+            self._yaw_min_pwm, 
+            self._yaw_max_pwm)
 
-        self._throttle = 1500
         self._throttle_min_pwm = 1100
         self._throttle_max_pwm = 1900
+        self._throttle = self.compute_pwm(
+            parameters.get("throttle", 0.5), 
+            self._throttle_min_pwm, 
+            self._throttle_max_pwm)
         
         self._apm_mode = 1146
         self._gimbal_yaw = self._NO_CHANGE
         self._gimbal_tilt = self._NO_CHANGE
         self._gimbal_roll = self._NO_CHANGE
 
-    def compute_pwm_signal(self, percent, minimum, maximum):
+    def compute_pwm(self, percent, minimum, maximum):
         if (percent < 0.0) or (percent > 1.0):
             raise ValueError
 
         return numpy.interp(percent, [0, 1.0], [minimum, maximum])
-
-    def set_roll(self, percent):
-        self._roll = self.compute_pwm_signal(
-            percent, self._roll_min_pwm, self._roll_max_pwm)
-
-    def set_pitch(self, percent):
-        self._pitch = self.compute_pwm_signal(
-            percent, self._pitch_min_pwm, self._pitch_max_pwm)
-    
-    def set_throttle(self, percent):
-        self._throttle = self.compute_pwm_signal(
-            percent, self._throttle_min_pwm, self._throttle_max_pwm)
-
-    def set_yaw(self, percent):
-        self._yaw = self.compute_pwm_signal(
-            percent, self._yaw_min_pwm, self._yaw_max_pwm)
 
     def to_roscopter(self):
         return [self._roll, self._pitch, self._yaw,
--- quadcopter_brain/src/quadcopter_brain/test_rc_command.py
+++ quadcopter_brain/src/quadcopter_brain/test_rc_command.py
@@ -4,70 +4,82 @@ import rc_command
 
 
 class TestRCCommand(unittest.TestCase):
-    def setUp(self):
-        self.rc_command = rc_command.RCCommand()
-
     def test_to_roscopter(self):
-        roscopter_command = self.rc_command.to_roscopter()
+        command = rc_command.RCCommand()
+        roscopter_command = command.to_roscopter()
         true_command = [1500, 1500, 1500, 1500, 1146, 65535, 65535, 65535]
         self.assertEqual(roscopter_command, true_command)
 
-    def test_compute_pwm_signal(self):
-        self.assertRaises(
-            ValueError, self.rc_command.compute_pwm_signal, 2.0, 1100, 1900)
-        self.assertRaises(
-            ValueError, self.rc_command.compute_pwm_signal, -1.0, 1100, 1900)
+        command = rc_command.RCCommand(
+            {"roll": 0.0, "pitch": 0.0, "yaw": 0.0, "throttle": 0.0})
+        roscopter_command = command.to_roscopter()
+        true_command = [1100, 1100, 1100, 1100, 1146, 65535, 65535, 65535]
+        self.assertEqual(roscopter_command, true_command)
 
-        pwm_signal = self.rc_command.compute_pwm_signal(0.0, 1100, 1900)
+        command = rc_command.RCCommand(
+            {"roll": 1.0, "pitch": 1.0, "yaw": 1.0, "throttle": 1.0})
+        roscopter_command = command.to_roscopter()
+        true_command = [1900, 1900, 1900, 1900, 1146, 65535, 65535, 65535]
+        self.assertEqual(roscopter_command, true_command)
+
+    def test_compute_pwm(self):
+        command = rc_command.RCCommand()
+
+        self.assertRaises(
+            ValueError, command.compute_pwm, 2.0, 1100, 1900)
+        self.assertRaises(
+            ValueError, command.compute_pwm, -1.0, 1100, 1900)
+
+        pwm_signal = command.compute_pwm(0.0, 1100, 1900)
         self.assertEqual(pwm_signal, 1100)
-        pwm_signal = self.rc_command.compute_pwm_signal(0.5, 1100, 1900)
+        pwm_signal = command.compute_pwm(0.5, 1100, 1900)
         self.assertEqual(pwm_signal, 1500)
-        pwm_signal = self.rc_command.compute_pwm_signal(1.0, 1100, 1900)
+        pwm_signal = command.compute_pwm(1.0, 1100, 1900)
         self.assertEqual(pwm_signal, 1900)
 
-    def test_set_throttle(self):
-        self.rc_command.set_throttle(0.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1500, 1500, 1500, 1100, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    # def test_set_throttle(self):
+    #     self.rc_command.set_throttle(0.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1500, 1500, 1500, 1100, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-        self.rc_command.set_throttle(1.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1500, 1500, 1500, 1900, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    #     self.rc_command.set_throttle(1.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1500, 1500, 1500, 1900, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-    def test_set_roll(self):
-        self.rc_command.set_roll(1.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1900, 1500, 1500, 1500, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    # def test_set_roll(self):
+    #     self.rc_command.set_roll(1.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1900, 1500, 1500, 1500, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-        self.rc_command.set_roll(0.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1100, 1500, 1500, 1500, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    #     self.rc_command.set_roll(0.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1100, 1500, 1500, 1500, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-    def test_set_pitch(self):
-        self.rc_command.set_pitch(1.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1500, 1900, 1500, 1500, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    # def test_set_pitch(self):
+    #     self.rc_command.set_pitch(1.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1500, 1900, 1500, 1500, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-        self.rc_command.set_pitch(0.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1500, 1100, 1500, 1500, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    #     self.rc_command.set_pitch(0.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1500, 1100, 1500, 1500, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-    def test_set_yaw(self):
-        self.rc_command.set_yaw(1.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1500, 1500, 1900, 1500, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    # def test_set_yaw(self):
+    #     self.rc_command.set_yaw(1.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1500, 1500, 1900, 1500, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
-        self.rc_command.set_yaw(0.0)
-        roscopter_command = self.rc_command.to_roscopter()
-        true_command = [1500, 1500, 1100, 1500, 1146, 65535, 65535, 65535]
-        self.assertEqual(roscopter_command, true_command)
+    #     self.rc_command.set_yaw(0.0)
+    #     roscopter_command = self.rc_command.to_roscopter()
+    #     true_command = [1500, 1500, 1100, 1500, 1146, 65535, 65535, 65535]
+    #     self.assertEqual(roscopter_command, true_command)
 
 
 if __name__ == '__main__':
