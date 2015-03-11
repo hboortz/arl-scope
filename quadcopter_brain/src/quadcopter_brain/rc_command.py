d42ade9fdd18ae6e7875261ab31824fbe7050abe
Elizabeth Duncan and Kyle McConnaughay <truepinkluv24+kyle.mcconnaughay@gmail.com>
2015-03-11 11:31:17 -0400

    renamed to_mavlink to to_roscopter

--- quadcopter_brain/src/quadcopter_brain/rc_command.py
+++ quadcopter_brain/src/quadcopter_brain/rc_command.py
@@ -1,5 +1,10 @@
+import numpy
+
+
 class RCCommand(object):
     def __init__(self):
+        self._throttle_min_pwm = 1100
+        self._throttle_max_pwm = 1900
         self._roll = 1500
         self._pitch = 1500
         self._yaw = 1500
@@ -9,8 +14,15 @@ class RCCommand(object):
         self._gimbal_tilt = -1
         self._gimbal_roll = -1
 
-    def to_mavlink(self):
+    def set_throttle(self, percent):
+        if (percent < 0.0) or (percent > 1.0):
+            raise ValueError
+
+        self._throttle = numpy.interp(percent, 
+            [0, 1.0], [self._throttle_min_pwm, self._throttle_max_pwm])
+
+    def to_roscopter(self):
         return "command: [%d, %d, %d, %d, %d, %d, %d, %d]" % (
-            self._roll, self._pitch, self._yaw, 
-            self._throttle, self._apm_mode, self._gimbal_yaw, 
+            self._roll, self._pitch, self._yaw,
+            self._throttle, self._apm_mode, self._gimbal_yaw,
             self._gimbal_tilt, self._gimbal_roll)
--- quadcopter_brain/src/quadcopter_brain/test_rc_command.py
+++ quadcopter_brain/src/quadcopter_brain/test_rc_command.py
@@ -7,10 +7,29 @@ class TestRCCommand(unittest.TestCase):
     def setUp(self):
         self.rc_command = rc_command.RCCommand()
 
-    def test_to_mavlink(self):
-        mavlink_command = self.rc_command.to_mavlink()
+    def test_to_roscopter(self):
+        roscopter_command = self.rc_command.to_roscopter()
         true_command = "command: [1500, 1500, 1500, 1500, 1146, -1, -1, -1]"
-        self.assertEqual(mavlink_command, true_command)    
+        self.assertEqual(roscopter_command, true_command)
+
+    def test_set_throttle(self):
+        self.assertRaises(ValueError, self.rc_command.set_throttle, 2.0)
+        self.assertRaises(ValueError, self.rc_command.set_throttle, -1.0)
+
+        self.rc_command.set_throttle(1.0)
+        roscopter_command = self.rc_command.to_roscopter()
+        true_command = "command: [1500, 1500, 1500, 1900, 1146, -1, -1, -1]"
+        self.assertEqual(roscopter_command, true_command)
+
+        self.rc_command.set_throttle(0.5)
+        roscopter_command = self.rc_command.to_roscopter()
+        true_command = "command: [1500, 1500, 1500, 1500, 1146, -1, -1, -1]"
+        self.assertEqual(roscopter_command, true_command)
+
+        self.rc_command.set_throttle(0.0)
+        roscopter_command = self.rc_command.to_roscopter()
+        true_command = "command: [1500, 1500, 1500, 1100, 1146, -1, -1, -1]"
+        self.assertEqual(roscopter_command, true_command)
 
 
 if __name__ == '__main__':
