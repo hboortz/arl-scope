b861b8d2f2d8d0abfe8b61688907ac16527a6196
Eric Schneider and Kaitlin Gallagher <franz.eric.schneider+kkgallagher9@gmail.com>
2015-03-28 13:31:28 -0400

    PEP8 compliance

--- quadcopter_brain/src/quadcopter_brain/rc_command.py
+++ quadcopter_brain/src/quadcopter_brain/rc_command.py
@@ -8,31 +8,31 @@ class RCCommand(object):
         self._roll_min_pwm = 1100
         self._roll_max_pwm = 1900
         self._roll = self.compute_pwm(
-            parameters.get("roll", 0.5), 
-            self._roll_min_pwm, 
+            parameters.get("roll", 0.5),
+            self._roll_min_pwm,
             self._roll_max_pwm)
-        
+
         self._pitch_min_pwm = 1100
         self._pitch_max_pwm = 1900
         self._pitch = self.compute_pwm(
-            parameters.get("pitch", 0.5), 
-            self._pitch_min_pwm, 
+            parameters.get("pitch", 0.5),
+            self._pitch_min_pwm,
             self._pitch_max_pwm)
-        
+
         self._yaw_min_pwm = 1100
         self._yaw_max_pwm = 1900
         self._yaw = self.compute_pwm(
-            parameters.get("yaw", 0.5), 
-            self._yaw_min_pwm, 
+            parameters.get("yaw", 0.5),
+            self._yaw_min_pwm,
             self._yaw_max_pwm)
 
         self._throttle_min_pwm = 1100
         self._throttle_max_pwm = 1900
         self._throttle = self.compute_pwm(
-            parameters.get("throttle", 0.5), 
-            self._throttle_min_pwm, 
+            parameters.get("throttle", 0.5),
+            self._throttle_min_pwm,
             self._throttle_max_pwm)
-        
+
         self._apm_mode = 1146
         self._gimbal_yaw = self._NO_CHANGE
         self._gimbal_tilt = self._NO_CHANGE
@@ -46,5 +46,5 @@ class RCCommand(object):
 
     def to_roscopter(self):
         return [self._roll, self._pitch, self._yaw,
-                self._throttle, self._apm_mode, self._gimbal_yaw, 
+                self._throttle, self._apm_mode, self._gimbal_yaw,
                 self._gimbal_tilt, self._gimbal_roll]
