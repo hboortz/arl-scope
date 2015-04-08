2f2e010d1b2b7e007b728f177be9a4ae63b18ac5
Eric Schneider and Kaitlin Gallagher <franz.eric.schneider+kkgallagher9@gmail.com>
2015-04-08 09:54:45 -0400

    Removing excess comment

--- quadcopter_brain/src/quadcopter_brain/test_quadcopter_brain.py
+++ quadcopter_brain/src/quadcopter_brain/test_quadcopter_brain.py
@@ -37,10 +37,6 @@ class TestQuadcopterBrain(unittest.TestCase):
         go_to_waypoints_mock.assert_called_once_with(waypoint_data)
         self.quadcopter_mock.land.assert_called_once_with()
 
-    def test_hover_in_place(self):
-        # TODO(Eric) Put a test here
-        pass
-
     @mock.patch('quadcopter_brain.QuadcopterBrain.go_to_waypoints')
     def test_go_to_waypoint_given_metered_offset(self, go_to_waypoint_mock):
         delta_east = 10  # Meters
