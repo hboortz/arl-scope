c1df64d58e3a81a3007c939cba91d8bb168f555f
Elizabeth Duncan and Kyle McConnaughay <truepinkluv24+kyle.mcconnaughay@gmail.com>
2015-04-15 17:48:01 -0400

    Fixing current mission.py

--- quadcopter_brain/src/quadcopter_brain/current_mission.py
+++ quadcopter_brain/src/quadcopter_brain/current_mission.py
@@ -4,82 +4,8 @@ import rospy
 
 from quadcopter import Quadcopter
 from rc_command import RCCommand
-
-
-def backward(q):
-    rc_command = RCCommand({'pitch': 0.9})
-    q.send_rc_command(rc_command)
-
-
-def forward(q):
-    rc_command = RCCommand({'pitch': 0.1})
-    q.send_rc_command(rc_command)
-
-
-def right(q):
-    rc_command = RCCommand({'roll': 0.9})
-    q.send_rc_command(rc_command)
-
-
-def left(q):
-    rc_command = RCCommand({'roll': 0.1})
-    q.send_rc_command(rc_command)
-
-
-def still(q):
-    rc_command = RCCommand()
-    q.send_rc_command(rc_command)
-
-
-def throttle_up(q):
-    rc_command = RCCommand({"throttle": 0.8})
-    q.send_rc_command(rc_command)
-
-
-def throttle_down(q):
-    rc_command = RCCommand({"throttle": 0.2})
-    q.send_rc_command(rc_command)
-
-
-def rc_square_dance():
-    import time
-
-    q = Quadcopter()
-    outside = rospy.get_param("Quadcopter/outside", False)
-    if outside:
-        q.arm()
-    q.launch()
-
-    print "forward"
-    forward(q)
-    time.sleep(2)
-    print "still"
-    still(q)
-    time.sleep(2)
-
-    print "right"
-    right(q)
-    time.sleep(2)
-    print "still"
-    still(q)
-    time.sleep(2)
-
-    print "backward"
-    backward(q)
-    time.sleep(2)
-    print "still"
-    still(q)
-    time.sleep(2)
-
-    print "left"
-    left(q)
-    time.sleep(2)
-    print "still"
-    still(q)
-    time.sleep(2)
-
-    throttle_down(q)
-    time.sleep(20)
+from waypoint_tools import WaypointTools
+from quadcopter_brain import QuadcopterBrain
 
 
 def print_position_data(quadcopter):
@@ -110,9 +36,9 @@ def main():
         carl.arm()
     carl.launch()
     found, _, _ = \
-        carl.find_landing_site_at_waypoints([great_lawn_waypoints['C5'],
-                                             great_lawn_waypoints['B5'],
-                                             great_lawn_waypoints['A5']])
+        carl.find_landing_site_at_waypoints([great_lawn_waypoints['C'],
+                                             great_lawn_waypoints['B'],
+                                             great_lawn_waypoints['C']])
     if found:
         carl.land_on_fiducial_incremental()
     else:
